1509f0163d8fe7a8c0dce375d98714965bacf10b
hsmajic <hsmajic1@etf.unsa.ba>
2019-05-20 12:46:13 +0200

    initial commit

--- src/ba/unsa/etf/rs/tutorijal8/Bus.java
+++ src/ba/unsa/etf/rs/tutorijal8/Bus.java
@@ -1,4 +1,95 @@
 package ba.unsa.etf.rs.tutorijal8;
 
 public class Bus {
+    private Integer idBus = null;
+    private String maker, series;
+    private int seatNumber;
+    private Driver driverOne = null;
+    private Driver driverTwo = null;
+
+
+public Bus(String maker, String series, int seatNumber){
+    this.maker = maker;
+    this.series = series;
+    this.seatNumber = seatNumber;
+}
+
+public Bus(Integer idBus, String maker, String series, int seatNumber){
+    this.idBus = idBus;
+    this.maker = maker;
+    this.series = series;
+    this.seatNumber = seatNumber;
+}
+
+    public Bus() {
+    }
+
+    public Integer getIdBus() {
+        return idBus;
+    }
+
+    public void setIdBus(Integer idBus) {
+        this.idBus = idBus;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Driver getDriverOne() {
+        return driverOne;
+    }
+
+    public void setDriverOne(Driver driverOne) {
+        this.driverOne = driverOne;
+    }
+
+    public Driver getDriverTwo() {
+        return driverTwo;
+    }
+
+    public void setDriverTwo(Driver driverTwo) {
+        this.driverTwo = driverTwo;
+    }
+
+    @Override
+    public String toString() {
+        String ime = "";
+        ime += this.maker + " " + this.series + " (br. sjedista: " + this.getSeatNumber() + " )";
+        if(driverOne != null){
+            ime += driverOne.toString();
+        }
+        if(driverTwo != null){
+            ime += driverTwo.toString();
+        }
+        return ime;
+    }
+
+    public boolean equals (Bus bus){
+    if(idBus != null) {
+        return (bus.getIdBus().equals(this.getIdBus()));
+    }
+        return false;
+    }
+
 }
--- src/ba/unsa/etf/rs/tutorijal8/Driver.java
+++ src/ba/unsa/etf/rs/tutorijal8/Driver.java
@@ -1,9 +1,90 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import java.time.LocalDate;
+
 public class Driver {
 
   private Integer idDriver = null;
+  private String firstName, lastName, jmb;
+  private LocalDate birthdate, hireDate;
 
+
+  public Driver() {
+  }
+
+  public Driver(String firstName, String lastName, String jmb, LocalDate birthdate, LocalDate hireDate){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.jmb = jmb;
+    this.birthdate = birthdate;
+    this.hireDate = hireDate;
+  }
+
+  public Driver(Integer idDriver, String firstName, String lastName, String jmb, LocalDate birthdate, LocalDate hireDate){
+    this.idDriver = idDriver;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.jmb = jmb;
+    this.birthdate = birthdate;
+    this.hireDate = hireDate;
+  }
+
+  public Integer getIdDriver() {
+    return idDriver;
+  }
+
+  public void setIdDriver(Integer idDriver) {
+    this.idDriver = idDriver;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getJmb() {
+    return jmb;
+  }
+
+  public void setJmb(String jmb) {
+    this.jmb = jmb;
+  }
+
+  public LocalDate getBirthdate() {
+    return birthdate;
+  }
+
+  public void setBirthdate(LocalDate birthdate) {
+    this.birthdate = birthdate;
+  }
+
+  public LocalDate getHireDate() {
+    return hireDate;
+  }
+
+  public void setHireDate(LocalDate hireDate) {
+    this.hireDate = hireDate;
+  }
+
+  @Override
+  public String toString() {
+    return " -> (" + this.getFirstName() + " " + this.getLastName() + " ( " + this.getJmb() + " ))";
+  }
+
+  public boolean equals (Driver driver){
+    return (driver.getJmb().equals(this.getJmb()));
+  }
 
 }
 
